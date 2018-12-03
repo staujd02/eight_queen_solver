b786dda67c00490594689ddecf4258f3fc51bbf8
Joel <staujd02@students.ipfw.edu>
2018-12-03 11:53:31 -0500

    green: cleaned backtracking

--- backtracking.py
+++ backtracking.py
@@ -3,26 +3,32 @@ from constraints import Constraints
 class BackTracking(object):
 
     def __init__(self):
-        self.set = set()
         self.constraints = Constraints()
 
     def findSolution(self, variables):
         self.backtracking(0, variables)
 
     def backtracking(self, level, variables):
-        pick = -1
+        selectedQueen = self.pickUnassignedQueenFrom(variables)
+        if selectedQueen == None:
+            return True
+        if self.problemSolved(level, selectedQueen, variables):
+            return True
+        selectedQueen.value = -1
+        return False
+    
+    def pickUnassignedQueenFrom(self, variables):
         for idx, queen in enumerate(variables.queens):
             if queen.value == -1:
-                pick = idx
-                break
-
-        if pick == -1:
-            return True
-        
-        for value in variables.queens[pick].domain:
-            variables.queens[pick].value = value
-            if self.constraints.verify(variables):
-                if self.backtracking(level + 1, variables):
-                    return True
-        variables.queens[pick].value = -1
+                return queen
+        return None 
+    
+    def problemSolved(self, level, pick, variables):
+        for value in pick.domain:
+            pick.value = value
+            if self.assignedValueLeadsToValidSolution(level, variables):
+                return True
         return False
+
+    def assignedValueLeadsToValidSolution(self, level, variables):
+        return self.constraints.verify(variables) and self.backtracking(level + 1, variables)
